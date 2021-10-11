d0c4ddb70117b4969473ad868777c23ee3dd74e3
Nils Matic <matic1909@gmail.com>
2021-10-11 10:52:45 +0200

    added styling to Joke component

--- src/components/Joke.js
+++ src/components/Joke.js
@@ -15,15 +15,42 @@ export default class Joke extends Component {
     this.props.handleVote(this.props.id, delta);
   }
 
+  getColor() {
+    const { score } = this.props;
+    if (score >= 15) return '#4caf50';
+    if (score >= 12) return '#8bc34a';
+    if (score >= 9) return '#cddc39';
+    if (score >= 6) return '#ffeb3b';
+    if (score >= 3) return '#ffc107';
+    if (score >= 0) return '#ff9800';
+    return '#f44336';
+  }
+
+  getEmoji() {
+    const { score } = this.props;
+    if (score >= 15) return 'em em-rolling_on_the_floor_laughing';
+    if (score >= 12) return 'em em-laughing';
+    if (score >= 9) return 'em em-smiley';
+    if (score >= 6) return 'em em-slightly_smiling_face';
+    if (score >= 3) return 'em em-neutral_face';
+    if (score >= 0) return 'em em-confused';
+    return 'em em-poop';
+  }
+
   render() {
     return (
       <div className="Joke">
         <div className="Joke-buttons">
           <i className="fas fa-arrow-up" onClick={this.handleVote}></i>
-          <span>{this.props.score}</span>
+          <span className="Joke-votes" style={{ borderColor: this.getColor() }}>
+            {this.props.score}
+          </span>
           <i className="fas fa-arrow-down" onClick={this.handleVote}></i>
         </div>
         <div className="Joke-text">{this.props.joke}</div>
+        <div className="Joke-emoji">
+          <i class={this.getEmoji()}></i>
+        </div>
       </div>
     );
   }
